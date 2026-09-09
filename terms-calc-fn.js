814249818fbc6365569bf1eefd732e6a7c98177e
sanchesefn <sanchesefn@gmail.com>
2026-09-09 18:45:30 +0400

    Restore terms-calc-fn.js: условия 10.09 и калькулятор КМ 03.09

--- terms-calc-fn.js
+++ terms-calc-fn.js
@@ -1,1 +1,12 @@
-PLACEHOLDER
+    const TERMS_DATE = "10.09.2026";
+    const KM_CORRIDOR = [
+      {name:"T4 2025",km:"\u221230 / 20",note:"старый T4"},
+      {name:"T4L любая",km:"0 / 50",note:"Active и Prime"},
+      {name:"T7 любая",km:"0 / 50",note:"2WD и 4WD"},
+      {name:"T8 приоритет",km:"\u221220 / 0",note:"VIN из списка приоритета"},
+      {name:"T8 остальные",km:"0 / 30",note:""},
+      {name:"Tiggo 9 приоритет",km:"\u221220 / 0",note:"VIN из списка приоритета"},
+      {name:"Tiggo 9 остальные",km:"0 / 30",note:""},
+      {name:"Arrizo 8 приоритет",km:"\u221230 / 0",note:"VIN из списка приоритета"},
+      {name:"Arrizo 8 остальные",km:"0 / 30",note:""}
+    ];
